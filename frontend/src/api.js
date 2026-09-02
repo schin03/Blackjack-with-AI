990ffbaff511e817797257c8acf601215ec8b64e
Sam Chin <thomasamc7@gmail.com>
2026-09-02 16:38:33 -0700

    updating urls for frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,5 @@
-const API_URL = "http://127.0.0.1:8000";
+// const API_URL = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 // create_game api call
 export async function create_game(bal) {
