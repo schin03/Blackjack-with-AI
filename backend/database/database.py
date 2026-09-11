e32e6f9c58eecf5610fd66ddb95b36b661cdc18f
Sam Chin <thomasamc7@gmail.com>
2026-09-11 12:08:41 -0700

    added postgresql imports

--- backend/database/database.py
+++ backend/database/database.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blackjack")
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 connect_args = {"check_same_thread" : False} if DATABASE_URL.startswith("sqlite") else {}
 
